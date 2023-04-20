9e4a8cb6862fb710599301af2f865c927077298d
triracle97 <triracle@gmail.com>
2023-04-20 18:52:48 +0700

    Use https

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const BACKEND_HOST =
   process.env.NODE_ENV === 'development'
     ? 'http://51.79.147.198:3000'
-    : 'http://api.namthanhdatholdings.com'
+    : 'https://api.namthanhdatholdings.com'
