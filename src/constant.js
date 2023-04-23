93f9d1e4435ffeb8b6fc2c55df45c6c6a0ac9097
Triracle97 <triracle@gmail.com>
2023-04-23 21:10:57 +0700

    Add product type

--- src/constant.js
+++ src/constant.js
@@ -2,3 +2,7 @@ export const BACKEND_HOST =
   process.env.NODE_ENV === 'development'
     ? 'http://51.79.147.198:3000'
     : 'https://api.namthanhdatholdings.com'
+
+export const productType = [
+  { value: 'datNen', name: 'Đất nền'}
+]
