83330751563faeb10ab9cc5f28be363918f2b5a3
hieu <hieule130997@gmail.com>
2023-05-02 06:06:16 +0700

    update routes

--- src/routes.js
+++ src/routes.js
@@ -57,7 +57,7 @@ const Product = React.lazy(() => import('./views/data/Product'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  // { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
   { path: '/theme/typography', name: 'Typography', element: Typography },
